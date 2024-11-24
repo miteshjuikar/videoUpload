99d3333550079bfe2b2b992b4630a98531938a17
miteshjuikar <106508740+miteshjuikar@users.noreply.github.com>
2024-11-24 08:58:48 +0530

    changes

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
-    cloud_name: 'dslqocpya', 
-    api_key: '497337285795762', 
-    api_secret: 'R9TvFIUViv5PSJ-AseWIujcQmq0'
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
